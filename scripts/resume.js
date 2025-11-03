fac952c313177173ccb57a75f4aa1f47182742fa
celwynGH <celwynalexispanis@gmail.com>
2025-11-03 09:40:06 +0800

    added resume html css and js

---
+++ scripts/resume.js
@@ -0,0 +1,11 @@
+// Placeholder for interactive features
+
+// Example: Print resume
+function printResume() {
+  window.print();
+}
+
+// You can trigger this with a button if you want:
+// <button onclick="printResume()">Print Resume</button>
+// Print function
+
